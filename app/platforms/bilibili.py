6366ec8f42075c05e4626943a14a8f46f284d2d0
Babatuos2007 <2607263248@qq.com>
2025-01-09 04:19:56 +0800

    fix(bilibili): 修复歌曲时长显示问题
    - 添加歌曲时长格式化方法，将 "M:S" 格式转换为 "MM:SS" 格式
    - 更新歌曲信息处理逻辑，使用新的时长格式化方法- 优化错误处理，增加异常捕获和错误信息返回

--- app/platforms/bilibili.py
+++ app/platforms/bilibili.py
@@ -41,7 +41,7 @@ class BilibiliSearch(BaseSearch):
                     "author": song["author"],  # 作者
                     "cover": f"https:{song['pic']}?param=224y224",  # 封面
                     "play": song["play"],  # 播放量
-                    "duration": song["duration"],  # 时长
+                    "duration": self.format_duration(song["duration"]),  # 时长
                     "url": song["arcurl"],  # 链接
                     "id": song["bvid"]
                 }
@@ -88,3 +88,23 @@ class BilibiliSearch(BaseSearch):
             return {"error": f"请求失败: {e}"}
         except Exception as e:
             return {"error": f"发生错误: {e}"}
+
+    @staticmethod
+    def format_duration(duration_str):
+        """
+        将格式为 "M:S" 的时间字符串转换为 "MM:SS" 格式。
+
+        :param duration_str: 格式为 "M:S" 的时间字符串
+        :return: 格式为 "MM:SS" 的时间字符串
+        """
+        try:
+            # 解析字符串以获取分钟和秒
+            minutes, seconds = map(int, duration_str.split(':'))
+
+            # 格式化时间为 MM:SS 格式
+            formatted_time = f"{minutes:02}:{seconds:02}"
+
+            return formatted_time
+        except ValueError:
+            # 如果格式不正确，返回默认值或记录错误信息
+            return "00:00"
