3df13584dc65a1b58b56029603fd34d217af8264
Babatuos2007 <2607263248@qq.com>
2025-01-31 00:11:51 +0800

    feat(KWMusic): 酷我接口未实现,先注释代码,后期更新

--- app/platforms/KWMusic/get_audio.py
+++ app/platforms/KWMusic/get_audio.py
@@ -1,3 +1,6 @@
+from app.platforms.utils import cookie_to_dict
+
+
 async def get_audio(self, audio_id):
     """
     实现获取音频功能
@@ -5,11 +8,16 @@ async def get_audio(self, audio_id):
     :param audio_id: 音频id
     :return: 音频文件/链接，歌词
     """
-    base_url = "https://www.kuwo.cn/api/v1/www/music/playUrl"
+    cookies = cookie_to_dict(self.headers["cookie"])
+    secret = self.getSecret(cookies["Hm_Iuvt_cdb524f42f23cer9b268564v7y735ewrq2324"])
+    self.headers["secrt"] = secret
+    base_url = "https://kuwo.cn/api/v1/www/music/playUrl"
     # 生成 reqId
     reqId = self.reqid()
+    # 更新请求头
+    self.headers["referer"] = f"https://kuwo.cn/play_detail/{audio_id}"
     # 构建请求 URL
-    url = f"{base_url}?mid={audio_id}&type=music&httpsStatus=1&reqId={reqId}&plat=web_www"
+    url = f"{base_url}?mid={audio_id}&type=music&httpsStatus=1&reqId={reqId}&plat=web_www&from"
     try:
         # 发送 GET 请求
         response = await self.client.get(url, headers=self.headers)
