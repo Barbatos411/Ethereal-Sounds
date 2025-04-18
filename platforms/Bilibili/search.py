251114223935aa3e228dab1a2e21d5db51954553
Babatuos2007 <2607263248@qq.com>
2025-04-18 20:14:47 +0800

    refactor(Bilibili): 更正封面图片地址

--- platforms/Bilibili/search.py
+++ platforms/Bilibili/search.py
@@ -29,8 +29,8 @@ async def search(self, keyword: str, page: int = 1, limit: int = 30):
             # 作者
             "author": song["author"],
             # 封面
-            "cover": f"http://127.0.0.1:6160/referer?platform=Bilibili&url=https:{song['pic']}",
-            "hd_cover": f"http://127.0.0.1:6160/referer?platform=Bilibili&url=https:{song['pic']}",
+            "cover": f"/referer?platform=Bilibili&url=https:{song['pic']}",
+            "hd_cover": f"/referer?platform=Bilibili&url=https:{song['pic']}",
             # 播放量
             "play": song["play"],
             # 时长
